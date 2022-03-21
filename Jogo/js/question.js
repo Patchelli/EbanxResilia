4d7303499c0e22813c7af4da8f38d6a1d699f4db
Patrick Patchelli <patrick.patchelli@hotmail.com>
2022-03-21 20:10:28 -0300

    jogo projeto

---
+++ Jogo/js/question.js
@@ -0,0 +1,24 @@
+let perguntas = [
+    easy  = [
+        {
+            idPeergunta: 5,
+            pergunta: 'VEREADOR',
+            respostaCerta : "sim",
+            peso :2 ,
+            tipo : "Tech"
+        },
+    ],
+
+    normal =[
+        {
+
+        }
+    ],
+    
+    hard = [
+        {
+
+        }
+    ]
+   
+]
